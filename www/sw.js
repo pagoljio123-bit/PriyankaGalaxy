13802c1de488e03e3bc1e9c9876297ebfca6722f
pagoljio123-bit <pagoljio123@gmail.com>
2026-02-06 19:55:46 +0530

    Add service worker for caching files

---
+++ www/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "priyanka-galaxy-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/index.css",
+  "./js/index.js",
+  "./img/logo.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
